3dfdbcf81c8afc49a51cd2457e82b770dec6b8dc
mesotokyo <meso@meso.tokyo>
2019-03-20 00:49:05 +0900

    fix header layout, and add sort feature

--- bomreader/bomreader.js
+++ bomreader/bomreader.js
@@ -2,6 +2,50 @@
 
 // <song-list> component
 const songListMethods = {
+  changeSortOrder: function changeSortOrder(index) {
+    if (index == this.sortKeyIndex) {
+      // change sort order
+      if (this.sortOrder == 'asc') {
+        this.sortOrder = 'desc';
+      } else {
+        this.sortOrder = 'asc';
+      }
+    } else {
+      // change sort index
+      this.sortKeyIndex = index;
+    }
+    // sort!
+    // define utility function
+    const order = this.sortOrder == 'asc' ? 1 : -1
+    var cmp = function cmp(a, b) {
+      if (a > b) return order;
+      if (a < b) return -order;
+      return 0;
+    }      
+
+    this.queue.songs.sort((a, b) => {
+      if (this.sortKeyIndex == 0) {
+        // sort by song name
+        return cmp(a.song, b.song);
+      } else if (this.sortKeyIndex == 1) {
+        // sort by status
+        return cmp(a.status, b.status);
+      } else {
+        // sort by part and status
+        const i = this.sortKeyIndex - 2;
+        var playerA = a.parts[i].player || "";
+        var playerB = b.parts[i].player || "";
+        if (a.parts[i].status == 'no-entry' || a.parts[i].status == 'blank') playerA = "";
+        if (b.parts[i].status == 'no-entry' || b.parts[i].status == 'blank') playerB = "";
+
+        const result1 = cmp(playerA, playerB);
+        if (result1 != 0) return result1;
+        // check status
+        return cmp(a.parts[i].status, b.parts[i].status);
+      }
+    });
+    
+  },
   toggleSongProperties: function toggleSongProperties(song) {
     if (!song.songID) { return; }
     if (song.showProperties === undefined) {
@@ -29,7 +73,7 @@ const songListMethods = {
 }
 
 Vue.component('song-list', {
-  data: function () { return {} },
+  data: function () { return { sortKeyIndex: 1, sortOrder: "asc" } },
   props: { queue: Object, eventId: String },
   template: '#song-list-template',
   methods: songListMethods,
